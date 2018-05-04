1c1b25bf30adc96010dbe5f8e2b9570116d463e3
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-04 08:36:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ['snickers', 'hudred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hudred grand', 'kitkat', 'skittles'];
 var addElementToBeginningOfArray(string); = {
   chocolateBars.unshift(string)
 }
